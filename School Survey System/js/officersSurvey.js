3de6dae568e0433fea28fa0f30dfc95169c82674
imqvictor <imavictor9@gmail.com>
2026-08-27 17:39:44 +0300

    updated school survery

--- School Survey System/js/officersSurvey.js
+++ School Survey System/js/officersSurvey.js
@@ -2,70 +2,148 @@ const questionnaires = getQuestionnaire();
 console.log(questionnaires);
 
 const dispayQuestionnaire = document.querySelector('.dispayQuestionnaire');
-let isOpen = false;
-
-dispayQuestionnaire.innerHTML = "";
-questionnaires.forEach(questionnaire => {
 
 
-    const qDiv = document.createElement('div');
-    qDiv.innerHTML = `
+function displayQuestionnaires() {
+
+    dispayQuestionnaire.innerHTML = "";
+    questionnaires.forEach(questionnaire => {
+        //set isOpen to false for each questinnaire
+        let isOpen = false;
+
+        const qDiv = document.createElement('div');
+        qDiv.innerHTML = `
     <h2>${questionnaire.questionnaire}</h2>
     `
 
-    const openBtn = document.createElement('button');
-    openBtn.textContent = "Open questionnaire";
-    openBtn.addEventListener('click', () => {
-        if (!isOpen) {
+        const openBtn = document.createElement('button');
+        openBtn.textContent = "Open questionnaire";
+        openBtn.addEventListener('click', () => {
+            if (!isOpen) {
 
-            //OPEN
-            isOpen = true;
+                //OPEN
+                isOpen = true;
 
-            //clear the current display
-            dispayQuestionnaire.innerHTML = "";
+                //clear the current display
+                dispayQuestionnaire.innerHTML = "";
 
-            //Questionnaire title and description
-            const title = document.createElement('h2');
-            title.textContent = questionnaire.questionnaire;
-            const description = document.createElement('p');
-            description.textContent = questionnaire.description;
+                //Questionnaire title and description
+                const title = document.createElement('h2');
+                title.textContent = questionnaire.questionnaire;
+                const description = document.createElement('p');
+                description.textContent = questionnaire.description;
 
-            dispayQuestionnaire.appendChild(title);
-            dispayQuestionnaire.appendChild(description);
+                dispayQuestionnaire.appendChild(title);
+                dispayQuestionnaire.appendChild(description);
 
-            //display every question
-            questionnaire.questions.forEach((quest, index) => {
-                const questionDiv = document.createElement('div');
-                questionDiv.innerHTML = `
-            <p>${index + 1}. ${quest.question}</p>
-             `
-                dispayQuestionnaire.appendChild(questionDiv);
-            });
+                //display every question
+                questionnaire.questions.forEach((quest, index) => {
 
-            //change the button to close
-            openBtn.textContent = "Close Questionnaire";
-            dispayQuestionnaire.appendChild(openBtn);
+                    const questionDiv = document.createElement('div');
+
+                    if (quest.type === "multiple-choice") {
+
+                        questionDiv.innerHTML = `
+                        <p>${index + 1}. ${quest.question}</p>
+                        `
+                        quest.choices.forEach(choice => {
+                            console.log(choice);
+                            const radioInput = document.createElement('input');
+                            radioInput.type = 'radio';
+                            radioInput.id = `choice-${quest.id}`;
+                            radioInput.name = `question-${quest.id}`;
+                            radioInput.value = choice;
+
+                            const label = document.createElement('label');
+                            label.htmlFor = `choice-${quest.id}`;
+                            label.textContent = choice;
+
+                            questionDiv.appendChild(radioInput);
+                            questionDiv.appendChild(label);
+                            questionDiv.appendChild(document.createElement('br'));
+                        });
+                    }
+
+                    if (quest.type === "short-text") {
+                        questionDiv.innerHTML = `
+                   <p>${index + 1}. ${quest.question}</p>
+                   `
+                        const textInput = document.createElement('input');
+                        textInput.type = "text";
+                        textInput.id = "text";
+
+                        questionDiv.appendChild(textInput);
+
+                    }
+
+                    if (quest.type === "yes/no") {
+                        questionDiv.innerHTML = `
+                   <p>${index + 1}. ${quest.question}</p>
+                   `
+                        quest.choices.forEach(choice => {
+                            const radio = document.createElement('input');
+                            radio.type = 'radio';
+                            radio.name = `question${quest.id}`;
+                            radio.id = `choice-${quest.id}`;
+                            radio.value = choice;
+
+                            const label = document.createElement('label');
+                            label.htmlFor = `choice-${quest.id}`;
+                            label.textContent = choice;
+
+                            questionDiv.appendChild(radio);
+                            questionDiv.appendChild(label);
+                            questionDiv.appendChild(document.createElement('br'));
+                        })
+
+                    }
+
+                    if (quest.type === "number") {
+                        questionDiv.innerHTML = `
+                   <p>${index + 1}. ${quest.question}</p>
+                   `
+                        const numBer = document.createElement('input');
+                        numBer.type = 'number';
+                        numBer.id = "no";
+
+                        quest.choices.push(numBer);
+
+                        questionDiv.appendChild(numBer);
+                        questionDiv.appendChild(document.createElement('br'));
+                    }
+
+                    dispayQuestionnaire.appendChild(questionDiv);
+                });
+
+                //change the button to close
+                openBtn.textContent = "Close Questionnaire";
+                dispayQuestionnaire.appendChild(openBtn);
 
 
-        } else {
-            isOpen = false;
+            } else {
+                //change what the button does
 
-            //change what the button does
-            //clear questionnaire
-            dispayQuestionnaire.innerHTML = "";
+                //CLOSE
+                isOpen = false;
 
-            //show questionnaire card again
-            dispayQuestionnaire.appendChild(qDiv);
-            dispayQuestionnaire.appendChild(openBtn);
+                //clear display card
+                dispayQuestionnaire.innerHTML = "";
 
-            //change button back to open
-            openBtn.textContent = "Open Questionnaire";
+                //change button back to open
+                openBtn.textContent = "Open Questionnaire";
 
-        }
+                //display the questionnaires
+                displayQuestionnaires();
+            }
+
+
+        });
+
+        dispayQuestionnaire.appendChild(qDiv);
+        dispayQuestionnaire.appendChild(openBtn);
 
     });
 
-    dispayQuestionnaire.appendChild(qDiv);
-    dispayQuestionnaire.appendChild(openBtn);
+}
 
-});
+displayQuestionnaires();
